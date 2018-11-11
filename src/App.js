adb851c70921bf4c08dd988526dc714d9f90b27e
Brownmagnus Olivers <brownmagnuso@gmail.com>
2018-11-11 07:38:07 -0500

    Display map using google-map-react api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import locations from './data/locations.json';
+import MapDisplay from './components/MapDisplay';
 
 class App extends Component {
-  render() {
+  state = {
+    lat: 38.963166 ,
+    lon: -76.9353225,
+    zoom: 13,
+    all: locations
+  }
+
+  render = () => {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div>
+          <h1>Brownia's Neighborhood Map</h1>
+        </div>
+        <div>
+          <MapDisplay
+            lat={this.state.lat}
+            lon={this.state.lon}
+            zoom={this.state.zoom}
+            locations={this.state.all}/>
+        </div>
       </div>
+
     );
   }
 }
