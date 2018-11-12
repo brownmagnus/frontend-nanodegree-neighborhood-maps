1f79f69e9085422da290c3073007566dde29b868
Brownmagnus Olivers <brownmagnuso@gmail.com>
2018-11-11 22:09:29 -0500

    Added button to the application

--- src/App.js
+++ src/App.js
@@ -8,13 +8,41 @@ class App extends Component {
     lat: 38.963166 ,
     lon: -76.9353225,
     zoom: 13,
-    all: locations
+    all: locations,
+    open: false
+  }
+
+  styles = {
+    menuButton: {
+      marginLeft: 10,
+      marginRight: 20,
+      position: "absolute",
+      left: 10,
+      top: 20,
+      backgroud: "white",
+      padding: 10
+    },
+    hide: {
+      display: "none"
+    },
+    header: {
+      marginTop: "0px"
+    }
+  };
+
+  toggleDisplay = () => {
+    this.setState({
+      open: !this.state.open
+    });
   }
 
   render = () => {
     return (
       <div className="App">
         <div>
+          <button onClick={this.toggleDisplay} style={this.styles.menuButton}>
+            <i className="fa fa-bars"></i>
+          </button>
           <h1>Brownia's Neighborhood Map</h1>
         </div>
         <div>
@@ -23,6 +51,7 @@ class App extends Component {
             lon={this.state.lon}
             zoom={this.state.zoom}
             locations={this.state.all}/>
+
         </div>
       </div>
 
